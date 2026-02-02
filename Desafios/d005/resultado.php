aefa47f8bebf8616610102bf3110e3f839838afb
Deyvison Ferreira <dfers22@gmail.com>
2026-02-02 20:04:41 -0300

    Analisador de Número Real

---
+++ Desafios/d005/resultado.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Analisador de Número Real</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Analisador de Número Real</h1>
+        <?php 
+            $numero = $_POST["num"] ?? 0 ;
+
+            echo "<p>Analisando o número <strong>". number_format($numero, 3, "," , ".") ."</strong> informado pelo usuário:</p>";
+
+            $inteiro = (int) $numero;
+            $fracionario = $numero - $inteiro;
+
+            echo "<ul><li> A parte inteira do número é: <strong>" . number_format($inteiro, 0, ",", ".") ."</strong><li>";
+            echo "<li> A parte fracionária do número é <strong>" . number_format($fracionario, 3, "," , ".") . "</strong></li></ul>";        
+        ?>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
+    </main>
+    
+</body>
+</html>
